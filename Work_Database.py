d547b01959c1ecab025d56d951188d52665296d9
Wes <wtreehaus@gmail.com>
2019-01-13 15:42:24 -0500

    Changed logic of menu and input functions

--- Work_Database.py
+++ Work_Database.py
@@ -12,8 +12,8 @@ db = SqliteDatabase('Work_Database.db')
 class Entry(Model):
     employee_name = CharField(max_length=255, unique=False)
     task_minutes = CharField(max_length=255, unique=False)
-    task_name = CCharField(max_length=255, unique=False)
-    task_date = DateTimeField(default=datetime.datetime.now)
+    task_name = CharField(max_length=255, unique=False)
+    task_date = DateTimeField()
     notes = CharField(max_length=255, unique=False)
 
     class Meta:
@@ -37,7 +37,7 @@ def main_menu():
 
     clear()
     while choice != 'q':
-        print("Welcome to the Worklog database. Enter 'q' to quit.")
+        print("Enter 'q' to quit.")
         for key, value in directory_main.items():
             print('{}) {}'.format(key, value.__doc__))
         choice = input('\n> ').lower().strip()
@@ -86,22 +86,26 @@ def view_entry(entries):
         Length: {}
         Notes: {}
         """.format(entry.employee_name,
-                entry.task_date,
-                entry.task_name,
-                entry.task_minutes,
-                entry.notes
-                ))
+                   entry.task_date,
+                   entry.task_name,
+                   entry.task_minutes,
+                   entry.notes
+                   ))
 
         print('n) next entry')
         print('d) delete entry')
+        print('m) main menu')
 
         next_action = None
 
         while next_action is None:
-            next_action = input(">: ").lower().strip()
+            next_action = input(">  ").lower().strip()
 
             if next_action == 'd':
                 delete_entry(entry)
+
+            elif next_action == 'm':
+                main_menu()
 
             elif next_action != 'n':
                 next_action = None
@@ -125,16 +129,20 @@ def add_entry():
     info4 = get_notes()
     clear()
 
-    Entry.create(employee_name=info1, task_name=info2, task_minutes=info3, notes=info4)
+    print("Add a new entry:")
+    info5 = get_task_date()
+    clear()
 
-    print("Saved successfully!")
+    Entry.create(employee_name=info1, task_name=info2, task_minutes=info3, notes=info4, task_date=info5)
+
+    print("Your entry has been saved.")
     input("Press ENTER to continue.")
 
 
 def get_employee_name():
     """Get the name of the employee"""
     while True:
-        employee = input("Enter the employee's name: ")
+        employee = input("Enter the name of the employee: ")
         if len(employee) == 0:
             print("\n You must enter a name")
             continue
@@ -153,6 +161,19 @@ def get_task_name():
             return task_name
 
 
+def get_task_date():
+    while True:
+        task_date = input("Enter a date for the task. Please use MM/DD/YYYY format: ")
+        try:
+            datetime.datetime.strptime(task_date, "%m/%d/%Y")
+        except ValueError:
+            clear()
+            print("Invalid date. Please provide a valid date using the format MM/DD/YYYY: ")
+            continue
+        else:
+            return task_date
+
+
 def get_time_spent():
     """Get the amount of time spent on a specific task"""
     while True:
@@ -168,79 +189,96 @@ def get_time_spent():
 
 def get_notes():
     """Get the optional notes from the task"""
-    notes = input("Please enter any additional notes for this entry (OPTIONAL): ")
+    notes = input("Please enter any additional notes for this task entry (OPTIONAL): ")
     return notes
 
 
 def find_by_employee():
     """Find employee based on entry"""
     entries = Entry.select().order_by(Entry.employee_name.desc())
-    print("Find by employee:\nSelect an employee from the list below:")
-    employees = []
+    if not entries:
+        input("The database is empty. Press ENTER to return to the main menu")
+        main_menu()
+    else:
+        print("Find by employee:\nSelect an employee from the list below:")
+        employees = []
+        for entry in entries:
+            if entry.employee_name not in employees:
+                employees.append(entry.employee_name)
 
-    for entry in entries:
-        if entry.employee_name not in employees:
-            employees.append(entry.employee_name)
+        for entry in employees:
+            print("{}.)  {}".format(employees.index(entry), str(entry)))
 
-    for entry in employees:
-        print("{}) {}".format(employees.index(entry), str(entry)))
-
-    selection = test_input(len(employees))
-    return entries.where(Entry.employee_name.contains(employees[selection]))
+        selection = test_input(len(employees))
+        return entries.where(Entry.employee_name.contains(employees[selection]))
 
 
 def find_by_date():
     """Find by date of the task"""
     entries = Entry.select().order_by(Entry.task_date.desc())
-    print("Find by date:\nSelect a date from the list below:")
-    date = []
+    if not entries:
+        input("The database is empty. Press ENTER to return to the main menu")
+        main_menu()
+    else:
+        date = []
+        print("Find by date:\nSelect a date from the list below:")
+        for entry in entries:
+            if entry.task_date not in date:
+                date.append(entry.task_date)
+        else:
+            for entry in date:
+                print("{}.)  {}".format(date.index(entry),
+                                        entry))
 
-    for entry in entries:
-        if entry.task_date not in date:
-            date.append(entry.task_date)
-
-    for entry in date:
-        print("{}) {}".format(date.index(entry),
-                              entry.strftime('%A %B %d, %Y %I:%Mp')))
-
-    selection = test_input(len(date))
-    return entries.where(Entry.task_date.contains(date[selection]))
+            selection = test_input(len(date))
+            return entries.where(Entry.task_date.contains(selection))
 
 
 def find_by_time_spent():
     """Find by time spent"""
     entries = Entry.select().order_by(Entry.task_date.desc())
-    print("Find by date:\nSelect a date from the list below:")
-    duration = []
+    if not entries:
+        input("The database is empty. Press ENTER to return to the main menu")
+        main_menu()
+    else:
+        duration = []
+        print("Find by date:\nSelect a date from the list below:")
+        for entry in entries:
+            if entry.task_minutes not in duration:
+                duration.append(entry.task_minutes)
 
-    for entry in entries:
-        if entry.task_minutes not in duration:
-            duration.append(entry.task_minutes)
+        for entry in duration:
+            print("{}.)  {}".format(duration.index(entry), entry))
 
-    for entry in duration:
-        print("{}) {}".format(duration.index(entry), entry))
-
-    selection = test_input(len(duration))
-    return entries.where(Entry.duration.contains(duration[selection]))
+        selection = test_input(len(duration))
+        return entries.where(Entry.task_minutes.contains(duration[selection]))
 
 
 def find_by_search_term():
     """Find by searching for a term"""
-    search_query = input("Enter a term to search the work log:\n> ")
-    entries = Entry.select().order_by(Entry.task_date.desc())
-    logs = entries.where(Entry.employee_name.contains(search_query)|
-                         Entry.task_name.contains(search_query)|
-                         Entry.notes.contains(search_query))
-
-    return logs
+    while True:
+        search_query = input("Enter a term to search the work log:\n> ")
+        entries = Entry.select().order_by(Entry.task_date.desc())
+        logs = entries.where(Entry.employee_name.contains(search_query)|
+                             Entry.task_name.contains(search_query)|
+                             Entry.notes.contains(search_query))
+        if logs:
+            return logs
+        else:
+            input("That term is not in the database. Press ENTER to return to the main menu")
+            main_menu()
 
 
 def delete_entry(entry):
     """Delete entry"""
     if input("Are you sure you want to delete this entry? Hit 'Y' for YES 'N' for NO ").upper() == 'Y':
         entry.delete_instance()
-        print('Entry has been deleted!')
+        print('Entry has been deleted.\n')
         input('Press ENTER to continue.')
+        clear()
+    else:
+        clear()
+        main_menu()
 
 
 def test_input(length):
@@ -259,16 +297,16 @@ def test_input(length):
 
 
 directory_main = OrderedDict([
-    ('1', add_entry),
-    ('2', view_loop),
+    ('a', add_entry),
+    ('b', view_loop),
     ])
 
 
 directory_view = OrderedDict([
-    ('1', find_by_employee),
-    ('2', find_by_date),
-    ('3', find_by_time_spent),
-    ('4', find_by_search_term)
+    ('a', find_by_employee),
+    ('b', find_by_date),
+    ('c', find_by_time_spent),
+    ('d', find_by_search_term)
     ])
 
 
